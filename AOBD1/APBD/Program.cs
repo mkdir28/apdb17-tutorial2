7dbb75c6f395d62c4ca82b3cb10d37e493259528
Maryna Kamienieva <mkdirwd@gmail.com>
2024-03-08 17:05:07 +0100

    Add GetAverage method

--- AOBD1/APBD/Program.cs
+++ AOBD1/APBD/Program.cs
@@ -5,3 +5,14 @@ Console.WriteLine("Modification 1");
 Console.WriteLine("Modification 2");
 Console.WriteLine("Modification 3");
 Console.WriteLine("Modification 4");
+
+static double GetAverage(int[] num)
+{
+    double sum = 0.0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        sum += num[i];
+    }
+
+    return sum / num.Length;
+}
