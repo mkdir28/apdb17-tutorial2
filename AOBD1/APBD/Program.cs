6c9a876a81bf97b3543b2bd73ce7fb19fb9e1a75
Maryna Kamienieva <mkdirwd@gmail.com>
2024-03-14 20:13:15 +0100

    add a static method on branch feature-max that takes an array of ints and returns the maximum value

--- AOBD1/APBD/Program.cs
+++ AOBD1/APBD/Program.cs
@@ -17,7 +17,21 @@ static double GetAverage(int[] num)
     return sum / num.Length;
 }
 
+//4 task - Rebase
+static double GetMax(int[] num)
+{
+    double max = Double.NegativeInfinity;
+    for (int i = 0; i < num.Length; i++)
+    {
+        if (num[i] > max)
+            max = num[i];
+    }
+    return max;
+}
+
 int[] num = {1,2,3,4};
 //we can use "var" (like in c++ auto)
 double avg = GetAverage(num);
+double max = GetMax(num);
 Console.WriteLine(avg);
+Console.WriteLine(max);
