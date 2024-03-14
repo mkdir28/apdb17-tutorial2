ff17443c1185d116a0b3fe76a5e79bcbe1da2f38
Maryna Kamienieva <mkdirwd@gmail.com>
2024-03-14 21:00:29 +0100

    modified the loop responsible for calculating the average -> sum_avg instead of sum

--- AOBD1/APBD/Program.cs
+++ AOBD1/APBD/Program.cs
@@ -8,13 +8,13 @@ Console.WriteLine("Modification 4");
 
 static double GetAverage(int[] num)
 {
-    double sum = 0.0;
+    double sum_avg = 0.0;
     for (int i = 0; i < num.Length; i++)
     {
-        sum += num[i];
+        sum_avg += num[i];
     }
 
-    return sum / num.Length;
+    return sum_avg / num.Length;
 }
 
 //4 task - Rebase
