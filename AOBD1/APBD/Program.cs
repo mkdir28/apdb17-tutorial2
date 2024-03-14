ef64d5457e9812ce43ed3e8dc24c13a8b9d009c8
Maryna Kamienieva <mkdirwd@gmail.com>
2024-03-14 21:33:37 +0100

    change GetAverage()

--- AOBD1/APBD/Program.cs
+++ AOBD1/APBD/Program.cs
@@ -8,13 +8,13 @@ Console.WriteLine("Modification 4");
 
 static double GetAverage(int[] num)
 {
-    double sum = 0.0;
+    double sums = 0.0;
     for (int i = 0; i < num.Length; i++)
     {
-        sum += num[i];
+        sums += num[i];
     }
 
-    return sum / num.Length;
+    return sums / num.Length;
 }
 
 //4 task - Rebase
